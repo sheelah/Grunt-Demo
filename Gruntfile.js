edffc29765ef67eab3580f4bc4f9f1a8803aa0f2
Sheelah Brennan <slb53@hotmail.com>
2015-10-17 11:44:27 -0600

    Add initial Grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+  });
+
+  // load plugins
+
+
+  // register tasks
+  grunt.registerTask('default', ['dev']);
+  grunt.registerTask('dev', []);
+
+};
