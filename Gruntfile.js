64c30c337643ed93827cb9e8c81433aacb5dd7dd
Sheelah Brennan <slb53@hotmail.com>
2015-10-17 11:44:27 -0600

    Add initial Grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    // watch for changes and trigger livereload
+    watch: {
+      },
+      options: {
+        livereload: true
+      }
+    },
+
+    // load plugins
+
+    // register tasks
+
+};
